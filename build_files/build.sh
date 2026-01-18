893ede84d9ad7473c7aa26307c74e1d211962fbb
Sourav Kumar Paul <theskpaul@proton.me>
2026-01-18 13:23:58 +0530

    Added VSCode

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,27 +3,11 @@
 set -ouex pipefail
 
 ### Install packages
+dnf5 config-manager setopt terra.enabled=1
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-
-dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 dnf5 install -y "https://repo.protonvpn.com/fedora-$(cat /etc/fedora-release | cut -d' ' -f 3)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.3-1.noarch.rpm"
-
-dnf5 makecache --refresh -y
-dnf5 install -y alacritty bat clang neovim docker-cli containerd docker-buildx docker-compose docker-compose-switch thefuck podman-compose godot mono-devel zsh zed git toolbox zsh-autosuggestions zsh-syntax-highlighting gh  --skip-unavailable
-dnf5 install -y https://code.visualstudio.com/sha/download?build=stable&os=linux-rpm-x64
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf5 install -y proton-vpn-gnome-desktop --skip-broken --skip-unavailable
+dnf5 install -y alacritty bat clang neovim docker-cli containerd docker-buildx docker-compose docker-compose-switch thefuck podman-compose godot mono-devel zsh zed git toolbox zsh-autosuggestions zsh-syntax-highlighting gh code btop --skip-unavailable
 
 #### Example for enabling a System Unit File
 
