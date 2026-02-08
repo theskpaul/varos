cad6022e22cbd95f897f7bed85eae1cfe6b4bcc2
Sourav Kumar Paul <theskpaul@proton.me>
2026-02-08 19:47:31 +0530

    Remove all packages other than "toolbox, clamav, clamtk, igt-gpu-tools (intel-gpu-tools), intel-media-driver, gparted, gh"

--- build_files/packages.sh
+++ build_files/packages.sh
@@ -3,57 +3,18 @@
 set -ouex pipefail
 
 ### Install packages
-dnf5 config-manager setopt terra.enabled=1
-dnf5 config-manager setopt rpmfusion-free.enabled=1
 dnf5 config-manager setopt rpmfusion-nonfree.enabled=1
 
 dnf5 remove -y libva-intel-media-driver
 
-PACKAGES="alacritty \
-          bat \
-          clang \
-          neovim \
-          gparted \
-          thefuck \
-          podman-compose \
-          godot \
-          zsh \
-          zed \
-          git \
-          toolbox \
-          zsh-autosuggestions \
-          zsh-syntax-highlighting \
-          gh \
-          code \
-          fuse-btrfs \
-          android-tools \
-          android-file-transfer \
-          google-android-emoji-fonts \
-          memtest86+ \
-          smartmontools \
-          plasma-firewall \
-          ksyslog \
-          kgpg \
-          kcalc \
-          okular \
-          gwenview \
-          persepolis \
-          clamav \
-          clamtk \
-          btop \
-          intel-gpu-tools \
-          intel-media-driver \
-          filelight \
-          terminus-fonts-console"
-          # docker \
-          # docker-cli \
-          # containerd \
-          # docker-buildx \
-          # docker-compose \
-          # docker-compose-switch \
+PACKAGES="toolbox \
+         clamav \
+         clamtk \
+         intel-gpu-tools \
+         intel-media-driver \
+         gparted \
+         gh"
 
 dnf5 install -y $PACKAGES --skip-unavailable
 
-dnf5 config-manager setopt terra.enabled=0
-dnf5 config-manager setopt rpmfusion-free.enabled=0
 dnf5 config-manager setopt rpmfusion-nonfree.enabled=0
